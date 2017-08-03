dafd2f6a6297514649a4930b33385c0e3b3b690f
Vincent Lonij <vincentropy 29819815+vincentropy@users.noreply.github.com>
2017-08-03 19:12:46 +0100

    added observe funtion to agent interface

--- battleground/agent.py
+++ battleground/agent.py
@@ -1,7 +1,11 @@
 
 class Agent(object):
+
     def __init__(self):
         pass
 
     def move(self,state):
+        raise NotImplementedError()
+
+    def observe(self,state):
         raise NotImplementedError()
