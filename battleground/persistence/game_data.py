78718e13fbeb67c1b17b897c4f61e1eab67a9fc7
Vincent Lonij <vincentropy 29819815+vincentropy@users.noreply.github.com>
2017-08-04 17:14:36 +0100

    implemented persistence based on couchdb

--- battleground/persistence/game_data.py
+++ battleground/persistence/game_data.py
@@ -1,10 +1,63 @@
+from uuid import uuid4
+from couchdb.client import Server
+import json
+
+def get_server():
+    s = Server()
+    return s
+
+def get_db(name=None,server=None):
+    if name is None:
+        name="game_states"
+    if server is None:
+        server=get_server()
+    if name in server:
+        db = server[name]
+    else:
+        db = server.create(name)
+    return db
+
+def get_new_id():
+    doc_id = uuid4().hex
+    return doc_id
+
+def save_game_state(game_id, sequence,game_type, player_ids, game_state,db=None):
+    if db is None:
+        db = get_db()
+    state_id = get_new_id()
+    doc = {"sequence":sequence,
+           "player_ids":json.dumps(player_ids),
+           "game_state":json.dumps(game_state),
+           "game_id":game_id,
+           "game_type":game_type,
+           "_id":state_id}
+
+    db.save(doc)
+    return state_id
 
 
-def save_game_state(game_id, game_state):
-    pass
+def save_game_history(game_type, player_ids, game_states,db=None):
+    game_id = get_new_id()
+    if db is None:
+        db = get_db()
 
-def save_game_history(game_type, player_ids, game_states):
-    pass
+    for i,state in enumerate(game_states):
+        save_game_state(game_id=game_id,
+                        sequence=i,
+                        game_type=game_type,
+                        player_ids=player_ids,
+                        game_state=state,
+                        db=db)
+    return game_id
 
-def load_game_history(game_id):
-    pass
+
+def load_game_history(game_id,db=None):
+    map_fun = '''function(doc) {emit([doc.game_id], doc)}'''
+    if db is None:
+        db = get_db()
+    result = db.query(map_fun)
+    data =  {r.id:r.value for r in result[[game_id]]}
+    for key,value in data.items():
+        game_state=data[key]["game_state"]
+        data[key]["game_state"] = json.loads(game_state)
+    return data
