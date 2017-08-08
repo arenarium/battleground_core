53711039495288d49bb3fe16df729dc45dc8ccf3
Vincent Lonij <vincentropy 29819815+vincentropy@users.noreply.github.com>
2017-08-08 21:51:47 +0100

    basic react app

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,12 @@ git clone git://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
 cp .oh-my-zsh/templates/zshrc.zsh-template .zshrc
 sudo chsh -s /bin/zsh ubuntu
 
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs
+sudo apt-get install build-essential
+sudo npm install npm@latest -g
+sudo npm install -g create-react-app
+
 sudo echo "
 source ~/python3/bin/activate
 cd /vagrant
