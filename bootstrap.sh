7f64c541d1b4c4eafc7b1a8b2bfd0a96ce2d3495
Vincent Lonij <vincentropy 29819815+vincentropy@users.noreply.github.com>
2017-08-17 18:55:52 +0100

    updated install in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,7 @@ cp .oh-my-zsh/templates/zshrc.zsh-template .zshrc
 sudo chsh -s /bin/zsh ubuntu
 
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install nodejs
-sudo apt-get install build-essential
+sudo apt-get -y install nodejs build-essential
 sudo npm install npm@latest -g
 sudo npm install -g create-react-app
 
