4be7c19d890c93bdb4d923c49dbfc877fb71f1c8
phylyc <0d26ae7a@opayq.com>
2017-08-27 13:43:54 +0100

    update test_bunnies. (not working! :( )

--- unit_tests/test_bunnies.py
+++ unit_tests/test_bunnies.py
@@ -7,7 +7,7 @@ num_players = 2
 
 # state = {"rollables": [1] * dice_game.NUM_DICE, "bunnies": [0] * dice_game.NUM_DICE, "hutches": [0] * dice_game.NUM_DICE, "movables": [1] * dice_game.NUM_DICE, "currentPlayer": 0, "scores": {0:0}, "extraBunnies": 0, "message": "", "allowedMoves": {"roll": 0, "stay": 0, "reset": 0, "moveBunny": 1, "moveHutch": 1}, "boardValue": 0,"lastPlayer": 2, "lastRound": False}
 
-state = {
+STATE = {
     "rollables": [1] * dice_game.NUM_DICE,
     "bunnies": [0] * dice_game.NUM_DICE,
     "hutches": [0] * dice_game.NUM_DICE,
@@ -16,7 +16,7 @@ state = {
     "scores": {0: 0},
     "extraBunnies": 0,
     "message": "",
-    "allowedMoves": {"roll": 0, "stay": 0, "reset": 0, "moveBunny": 1, "moveHutch": 1},
+    "allowedMoves": {"roll": 0, "stay": 0, "reset": 0, "moveBunny": 1, "moveHutch": 0},
     "boardValue": 0,
     "lastPlayer": num_players,
     "lastRound": False
@@ -24,8 +24,9 @@ state = {
 
 
 def test_reset():
-    engine = dice_game.DiceGame(state)
+    engine = dice_game.DiceGame(num_players=num_players, type="Bunnies", state=STATE)
     engine.reset()
+    state = copy.deepcopy(engine.get_state())
     # check reset properties
     assert state["rollables"].count(0) == 0
     assert state["bunnies"] == [0] * dice_game.NUM_DICE
@@ -36,18 +37,45 @@ def test_reset():
     assert state["extraBunnies"] == 0
     assert state["message"] == ""
     assert (state["allowedMoves"] == {"roll": 0, "stay": 1, "reset": 0, "moveBunny": 0, "moveHutch": 0}
-            or state["allowedMoves"] == {"roll": 0, "stay": 0, "reset": 0, "moveBunny": 1, "moveHutch": 1})
+            or state["allowedMoves"] == {"roll": 0, "stay": 0, "reset": 0, "moveBunny": 1, "moveHutch": 0})
     assert state["boardValue"] == 0
     assert state["lastPlayer"] == num_players
-    assert state["lastRound"] is False
+    assert not state["lastRound"]
 
 
 def test_score():
-    assert True
+    engine = dice_game.DiceGame(num_players=2, type="Bunnies", state=STATE)
+    engine.reset()
+    state = copy.deepcopy(engine.get_state())
+    # create state to check scoring
+    state["rollables"] = [0] * dice_game.NUM_DICE
+    state["bunnies"] = [1, 1, 1, 2, 0, 0, 0]
+    state["hutches"] = [0, 0, 0, 0, 2, 3, 4]
+    state["movables"] = [0] * dice_game.NUM_DICE
+    state["extraBunnies"] = 43
+    state["allowedMoves"] = {"roll": 1, "stay": 1, "reset": 0, "moveBunny": 0, "moveHutch": 0}
+    engine.move({"name": "stay",
+                 "value": 0})
+    assert 0 in state["scores"]
+    assert state["scores"][0] == 43 + (10 + 3) * 4
+    assert state["boardvalue"] == 43 + (10 + 3) * 4
+    engine.move({"name": "reset",
+                 "value": 0})
+    assert state["extraBunnies"] == 0
+    if state["allowedMoves"]["moveBunny"] == 1:
+        state["rollables"] = [6, 1, 2, 4, 5, 5, 6]
+        engine.move({"name": "moveBunny",
+                     "value": 1})
+    engine.move({"name": "stay",
+                 "value": 0})
+    assert 1 in state["scores"]
+    assert state["scores"][1] <= 1
 
 
 def test_do_roll():
-    engine = dice_game.DiceGame(state)
+    engine = dice_game.DiceGame(num_players=num_players, type="Bunnies", state=STATE)
+    engine.reset()
+    state = copy.deepcopy(engine.get_state())
     # set a state with only bunnies, no rollables
     state["rollables"] = [0] * dice_game.NUM_DICE
     state["bunnies"] = [1] * dice_game.NUM_DICE
@@ -72,7 +100,9 @@ def test_do_stay():
 
 
 def test_do_reset():
-    engine = dice_game.DiceGame(state)
+    engine = dice_game.DiceGame(num_players=num_players, type="Bunnies", state=STATE)
+    engine.reset()
+    state = copy.deepcopy(engine.get_state())
     # set a state that was just passed on
     state["rollables"] = [0, 5, 4, 6, 0, 4, 0]
     state["bunnies"] = [1, 0, 0, 0, 1, 0, 0]
@@ -94,13 +124,17 @@ def test_do_reset():
 
 def test_move_bunnies():
     # create state without bunnies or correct hutches (2 needs to be the first hutch)
-    engine = dice_game.DiceGame(state)
+    engine = dice_game.DiceGame(num_players=num_players, type="Bunnies", state=STATE)
+    engine.reset()
+    state = copy.deepcopy(engine.get_state())
     state["rollables"] = [random.randint(3, 6) for x in state["rollables"]]
     # try to move a die into the bunny container
     engine.move({"name": "moveBunny",
                  "value": random.randint(0, dice_game.NUM_DICE - 1)})
     # check that hutch didn't move into bunnies container:
     assert state["bunnies"] == [0] * dice_game.NUM_DICE
+    # check that allowed moves didn't change
+    assert state["allowedMoves"] == {"roll": 0, "stay": 0, "reset": 0, "moveBunny": 1, "moveHutch": 1}
 
     # create state with only bunnies
     state["rollables"] = [random.randint(1, 2) for x in state["rollables"]]
@@ -115,7 +149,10 @@ def test_move_bunnies():
 
 def test_move_hutches():
     # create state without hutches
-    engine = dice_game.DiceGame(state)
+    engine = dice_game.DiceGame(num_players=num_players, type="Bunnies", state=STATE)
+    engine.reset()
+    state = copy.deepcopy(engine.get_state())
+    state["rollables"] = [1] * dice_game.NUM_DICE
     # try to move a die into the hutch container
     engine.move({"name": "moveHutch",
                  "value": random.randint(0, dice_game.NUM_DICE - 1)})
@@ -139,10 +176,39 @@ def test_move_hutches():
     assert state["hutches"].count(0) == dice_game.NUM_DICE - 1
 
 
-def test_game_over():
+def test_last_round():
     # check ending condition
-    assert True
+    engine = dice_game.DiceGame(num_players=2, type="Bunnies", state=STATE)
+    engine.reset()
+    state = copy.deepcopy(engine.get_state())
+    # set a state that leads to END_SCORE, triggering the last round
+    player_0 = state["currentPlayer"]
+    state["rollables"] = [6, 1, 2, 4, 5, 5, 6]
+    state["allowedMoves"] = {"roll": 0, "stay": 0, "reset": 0, "moveBunny": 1, "moveHutch": 1}
+    state["scores"][player_0] = dice_game.END_SCORE - 1
+    engine.move({"name": "moveBunny",
+                 "value": 1})
+    engine.move({"name": "stay",
+                 "value": 0})
+    print(state["scores"])
+    assert state["scores"][player_0] == dice_game.END_SCORE
+    assert state["lastRound"]
+    assert state["lastPlayer"] == player_0
+    engine.move({"name": "reset",
+                 "value": 0})
+    if state["allowedMoves"]["moveBunny"] == 1:
+        state["rollables"] = [6, 1, 2, 4, 5, 5, 6]
+        engine.move({"name": "moveBunny",
+                     "value": 1})
+    engine.move({"name": "stay",
+                 "value": 0})
+    assert state["lastRound"]
+    assert state["lastPlayer"] == player_0
+    assert state["allowedMoves"] == {"roll": 0, "stay": 0, "reset": 0, "moveBunny": 0, "moveHutch": 0}
 
+
+def test_game_over():
+    assert True
 
     # check consecutiveness of hatches
     # check consecutiveness of players
@@ -157,6 +223,7 @@ if __name__ == "__main__":
     test_do_reset()
     test_move_bunnies()
     test_move_hutches()
+    test_last_round()
     test_game_over()
 
 
