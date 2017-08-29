929cdca9d600aa721c1fec431b11b7c6a603f920
phylyc <0d26ae7a@opayq.com>
2017-08-29 01:12:55 +0100

    updating test_arena

--- unit_tests/test_arena.py
+++ unit_tests/test_arena.py
@@ -1,6 +1,8 @@
 from battleground.game_runner import GameRunner
 
-from games.arena.arena_game import ArenaGameEngine
+from games.arena.arena import ArenaGameEngine
+from games.arena.arena_game_east import EASTArenaGameEngine
+from games.arena.arena_game_eq import EQArenaGameEngine
 from games.arena.dungeon import Dungeon
 from games.arena.gladiator import Gladiator
 from games.arena import arena_agent
@@ -12,7 +14,18 @@ import random
 def test_engine():
     age = ArenaGameEngine(num_players=2, type="Arena_1", size=[20, 20])
     assert age.get_game_name() == "Arena_1"
+    assert isinstance(age.gladiators, dict)
+    assert len(age.gladiators) == 2
     assert isinstance(age.dungeon, Dungeon)
+
+
+def test_EAST_engine():
+    age = EASTArenaGameEngine(num_players=2, type="Arena_1", size=[20, 20])
+    assert True
+
+
+def test_EQ_engine():
+    age = EQArenaGameEngine(num_players=2, type="Arena_1", size=[20, 20])
     assert isinstance(age.event_queue, deque)
 
 
@@ -26,15 +39,18 @@ def test_dungeon():
 
 def test_gladiator():
     pos = [random.randint(1, 100), random.randint(1, 100)]
-    stats = {"str": random.randint(1, 10),
-             "dex": random.randint(1, 10),
-             "con": random.randint(1, 10)}
-    skills = {"melee": random.randint(1, 10),
-              "eva": random.randint(1, 10),
-              "speed": random.randint(1, 10)}
-    glad = Gladiator(pos=pos, name="Maximus", stats=stats, skills=skills)
+    team = random.randint(1, 10)
+    stats = {"str": random.randint(1, 3),
+             "dex": random.randint(1, 3),
+             "con": random.randint(1, 3)}
+    skills = {"melee": random.randint(1, 3),
+              "eva": random.randint(1, 3),
+              "speed": random.randint(1, 3)}
+    glad = Gladiator(pos=pos, name="Maximus", team=team, stats=stats, skills=skills)
+    assert isinstance(glad.pos, list)
     assert glad.pos == pos
     assert glad.name == "Maximus"
+    assert glad.team == team
     assert glad.base_stats == stats
     assert glad.base_skills == skills
     assert isinstance(glad.get_stats(), dict)
@@ -46,7 +62,14 @@ def test_gladiator():
     assert isinstance(glad.get_max_hp(), int)
     assert isinstance(glad.max_hp, int)
     assert isinstance(glad.cur_hp, int)
+    assert isinstance(glad.get_max_sp(), int)
+    assert isinstance(glad.max_sp, int)
+    assert isinstance(glad.cur_sp, int)
     assert 0 < glad.get_speed()
+    assert isinstance(glad.boosts, dict)
+    assert glad.set_boosts({"speed": random.randint(1, 3)}) is None
+    assert glad.boosts["speed"] > 0
+    assert glad.move([1, 2]) is None
 
 
 def test_player():
