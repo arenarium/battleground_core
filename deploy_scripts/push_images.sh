8710680ecbe0124556618cab314d3a4547297344
Vincent Lonij <vincentropy 29819815+vincentropy@users.noreply.github.com>
2017-09-02 12:33:30 +0100

    configure travis to push to docker hub

---
+++ deploy_scripts/push_images.sh
@@ -0,0 +1,5 @@
+docker login -u $DOCKER_USER -p $DOCKER_API_KEY
+docker push battleground/uwsgi:latest
+docker push battleground/nginx:latest
+docker push battleground/uwsgi:latest
+docker push battleground/battleground:latest
