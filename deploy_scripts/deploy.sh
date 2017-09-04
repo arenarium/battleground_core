cb1581d9081bcfec917dea8212c20af22ab7661f
Vincent Lonij <vincentropy 29819815+vincentropy@users.noreply.github.com>
2017-09-04 08:36:01 +0100

    automatic deployment by travis

---
+++ deploy_scripts/deploy.sh
@@ -0,0 +1,3 @@
+docker-compose -f docker-compose-ui.yml -f docker-compose.prod.yml down
+docker-compose -f docker-compose-ui.yml -f docker-compose.prod.yml pull
+docker-compose -f docker-compose-ui.yml -f docker-compose.prod.yml up
