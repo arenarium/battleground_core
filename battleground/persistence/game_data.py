acf9ff660f32e67184a059d8fd61130da16019f5
Vincent Lonij <vincentropy 29819815+vincentropy@users.noreply.github.com>
2017-09-08 18:06:32 +0100

    added mongo to requirements.txt. tests passing.

--- battleground/persistence/game_data.py
+++ battleground/persistence/game_data.py
@@ -1,28 +1,35 @@
 from uuid import uuid4
-from couchdb.client import Server
+from pymongo import MongoClient
 import json
 from os import environ
 
-
-def get_server():
-    if "COUCHDB_HOST" in environ:
-        host = environ["COUCHDB_HOST"]
-        s = Server(host)
-    else:
-        s=Server()
-    return s
+global_client = None
 
 
-def get_db(name=None,server=None):
+def get_client():
+    global global_client
+    if global_client is None:
+        if "MONGO_HOST" in environ:
+            host = environ["MONGO_HOST"]
+            global_client = MongoClient(host)
+        else:
+            global_client = MongoClient()
+    return global_client
+
+
+def get_db(name=None, client=None):
     if name is None:
-        name="game_states"
-    if server is None:
-        server=get_server()
-    if name in server:
-        db = server[name]
-    else:
-        db = server.create(name)
+        name = "game_states"
+    if client is None:
+        client = get_client()
+    db = client[name]
     return db
+
+
+def get_collection(name=None):
+    if name is None:
+        name = "game_states"
+    return get_db()[name]
 
 
 def get_new_id():
@@ -30,33 +37,35 @@ def get_new_id():
     return doc_id
 
 
-def save_game_state(game_id,
-                    sequence,
-                    game_type,
-                    game_state,
-                    db=None):
+def save_game_states(game_id,
+                     game_type,
+                     game_states,
+                     collection=None):
     """
-    save one document to the data-store.
-    game_state: dict
+    save one or more documents to the data-store.
+    game_states: [dict,...]
         each key, value will be stord as key: json(value) in the document.
         expected keys are "game_state", "last_move" and "player_ids"
     """
-    if db is None:
-        db = get_db()
-    state_id = get_new_id()
-    doc = {"sequence":sequence,
-           "game_id":game_id,
-           "game_type":game_type,
-           "_id":state_id}
-    for key,value in game_state.items():
-        assert key not in doc
-        doc[key] = json.dumps(value)
+    if collection is None:
+        collection = get_collection()
+    all_docs = []
+    for i, game_state in enumerate(game_states):
+        doc = {
+            "sequence": i,
+            "game_id": game_id,
+            "game_type": game_type
+        }
+        for key, value in game_state.items():
+            assert key not in doc
+            doc[key] = json.dumps(value)
+        all_docs.append(doc)
 
-    db.save(doc)
-    return state_id
+    result = collection.insert_many(all_docs)
+    return result
 
 
-def save_game_history(game_type, game_states,db=None):
+def save_game_history(game_type, game_states, collection=None):
     """
     save a sequence of documents to the data-store.
     game_states: array of dict
@@ -66,48 +75,47 @@ def save_game_history(game_type, game_states,db=None):
     """
 
     game_id = get_new_id()
-    if db is None:
-        db = get_db()
+    if collection is None:
+        collection = get_collection()
 
-    for i,state in enumerate(game_states):
-        save_game_state(game_id=game_id,
-                        sequence=i,
-                        game_type=game_type,
-                        game_state=state,
-                        db=db)
+    result = save_game_states(game_id=game_id,
+                     game_type=game_type,
+                     game_states=game_states,
+                     collection=collection)
     return game_id
 
 
-def load_game_history(game_id,db=None):
+def load_game_history(game_id, collection=None):
     """load all states with the same game ID and return an ordered sequence"""
 
-    map_fun = '''function(doc) {emit([doc.game_id], doc)}'''
-    if db is None:
-        db = get_db()
-    result = db.query(map_fun)
-    data = {r.id:r.value for r in result[[game_id]]}
-    states_in_sequence = [None]*len(data)
+    if collection is None:
+        collection = get_collection()
+    result = collection.find({"game_id":game_id})
+    data = result[:]
+    states_in_sequence = [None] * result.count()
     """now decode some of the values that are json strings"""
-    for key,loaded_doc in data.items():
-        output_doc = loaded_doc.copy()
-        for data_key in ["game_state", "last_move"]:#decode these two keys, because they are special
-            output_doc[data_key] = json.loads(loaded_doc[data_key])
-        states_in_sequence[loaded_doc["sequence"]] = output_doc
+    for loaded_doc in data:
+        output_doc = {}
+        for data_key in loaded_doc:
+            if data_key in ["game_state", "last_move"]:  # decode these two keys, because they are special
+                output_doc[data_key] = json.loads(loaded_doc[data_key])
+            else:
+                output_doc[data_key] = loaded_doc[data_key]
+        states_in_sequence[output_doc["sequence"]] = output_doc
     return states_in_sequence
 
 
-def get_games_list(game_type=None, db=None):
+def get_games_list(game_type=None, collection=None):
     """
     get a list of unique game IDs
     """
 
-    if db is None:
-        db = get_db()
-    map_fun = '''function(doc) {emit([doc.game_type,doc.game_id])}'''
-    result = db.query(map_fun,'_count', group=True)
+    if collection is None:
+        collection = get_collection()
 
     if game_type is None:
-        data =  [(r.key,r.value) for r in result[:]]
+        result = collection.distinct(key="game_id")
     else:
-        data =  [(r.key,r.value) for r in result[[game_type]:[game_type,"ZZZ"]]]
-    return data
+        result = collection.distinct(key="game_id", filter={"game_type":game_type})
+
+    return result
--- unit_tests/test_persistence.py
+++ unit_tests/test_persistence.py
@@ -4,94 +4,95 @@ from couchdb.client import Server
 import uuid
 import random
 
-@pytest.fixture(scope="module")
-def db():
-    """temporary database for testing"""
 
-    s = game_data.get_server()
-    db = game_data.get_db("test_db",s)
-    yield db
-    del s["test_db"]
+@pytest.fixture(scope="module")
+def collection():
+    """temporary database for testing"""
+    c = game_data.get_client()
+    db = game_data.get_db("test_db")
+    yield db.game_states
+    c.drop_database("test_db")
+
 
 def test_connection():
-    s = game_data.get_server()
-    version = s.version()
-    print(version)
-    assert isinstance(version,str)
+    c = game_data.get_client()
+    connection_info = c.server_info()
+    print(connection_info)
+    assert isinstance(connection_info, dict)
+
 
 def test_game_id():
     """generate a unique ID"""
 
     id1 = game_data.get_new_id()
-    assert isinstance(id1,str)
-    assert len(id1)==32
+    assert isinstance(id1, str)
+    assert len(id1) == 32
     id2 = game_data.get_new_id()
-    assert isinstance(id2,str)
-    assert len(id2)==32
+    assert isinstance(id2, str)
+    assert len(id2) == 32
     assert id1 != id2
 
-def test_save_state(db):
+
+def test_save_state(collection):
     """save a single game state"""
 
-    test_state = {"game_state":{"k_a":"v_a","k_b":2,"k_c":None},
-                  "last_move":{"move":"a move"}}
+    test_state = {"game_state": {"k_a": "v_a", "k_b": 2, "k_c": None},
+                  "last_move": {"move": "a move"}}
     game_id = game_data.get_new_id()
-    state_id = game_data.save_game_state(game_id, 0,"test_game",test_state,db=db)
-    assert isinstance(state_id,str)
-    assert len(state_id)==32
-    loaded_states = game_data.load_game_history(game_id,db=db)
-    assert len(loaded_states)==1
+    state_ids = game_data.save_game_states(
+        game_id, "test_game", [test_state], collection=collection).inserted_ids
+    assert len(state_ids) == 1
 
-    assert state_id == loaded_states[0]["_id"]
-    for key,value in test_state.items():
-        assert isinstance(loaded_states[0][key],dict)
-        for key2,value2 in value.items():
+    loaded_states = game_data.load_game_history(game_id, collection=collection)
+    assert len(loaded_states) == 1
+
+    assert state_ids[0] == loaded_states[0]["_id"]
+    for key, value in test_state.items():
+        assert isinstance(loaded_states[0][key], dict)
+        for key2, value2 in value.items():
             assert loaded_states[0][key][key2] == value2
 
 
-
-def test_save_game_history(db):
+def test_save_game_history(collection):
     """save a sequence of game states"""
 
     test_states = []
     for i in range(10):
-        test_states.append({"game_state":{"k_a":random.randint(0,1000)},
-                           "last_move":{"k_move":random.randint(0,1000)}})
+        test_states.append({"game_state": {"k_a": random.randint(0, 1000)},
+                            "last_move": {"k_move": random.randint(0, 1000)}})
 
-    game_id = game_data.save_game_history("test_game",test_states,db=db)
+    game_id = game_data.save_game_history("test_game", test_states, collection=collection)
 
-    loaded_states = game_data.load_game_history(game_id,db=db)
+    loaded_states = game_data.load_game_history(game_id, collection=collection)
 
-    assert len(loaded_states)==10
+    assert len(loaded_states) == 10
 
-    #sequence_states = loaded_states#{s["sequence"]:s["game_state"] for s in loaded_states.values()}
+    # sequence_states = loaded_states#{s["sequence"]:s["game_state"] for s in
+    # loaded_states.values()}
 
-    for i,state in enumerate(test_states):
+    for i, state in enumerate(test_states):
         assert state["game_state"] == loaded_states[i]["game_state"]
         assert state["last_move"] == loaded_states[i]["last_move"]
 
 
-def test_game_list(db):
+def test_game_list(collection):
     """get list of games"""
 
-    data = game_data.get_games_list(db=db)
-    assert len(data)>0
-    assert len(data[0])==2
-    assert len(data[0][0])==2
-    assert data[0][1]%1 == 0 # is int
+    data = game_data.get_games_list(collection=collection)
+    assert len(data) > 0
+    assert len(data[0]) == 32
 
 
-def test_game_list_selector(db):
+def test_game_list_selector(collection):
     """get list of games"""
 
-    data = game_data.get_games_list(db=db,game_type="teskdsajhasde")
-    assert len(data)==0
+    data = game_data.get_games_list(collection=collection, game_type="teskdsajhasde")
+    assert len(data) == 0
 
-    data = game_data.get_games_list(db=db,game_type="test_game")
-    assert len(data)>0
-    assert len(data[0])==2
-    assert len(data[0][0])==2
-    assert data[0][1]%1 == 0 # is int
+    data = game_data.get_games_list(collection=collection, game_type="test_game")
+    assert len(data) > 0
+    assert len(data[0]) == 32
+
 
 
 if __name__ == "__main__":
