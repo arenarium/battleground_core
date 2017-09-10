4b48c1c3a6775dfe63b1c65a927ccc0b2237da85
Vincent Lonij <vincentropy 29819815+vincentropy@users.noreply.github.com>
2017-09-10 11:00:19 +0100

    added shebang

--- deploy_scripts/do_deploy.sh
+++ deploy_scripts/do_deploy.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 ./deploy_scripts/push_images.sh
 ssh -o "StrictHostKeyChecking no" -i deploy_key $DEPLOY_HOST ./deploy.sh
--- deploy_scripts/push_images.sh
+++ deploy_scripts/push_images.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker login -u $DOCKER_USER -p $DOCKER_API_KEY
 docker push battleground/uwsgi:latest
 docker push battleground/nginx:latest
