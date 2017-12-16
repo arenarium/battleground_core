8c8b6b3969ebabcbb0c5dc65e4f98ee7e590ac08
Vincent Lonij <vincentropy 29819815+vincentropy@users.noreply.github.com>
2017-12-16 13:58:04 +0000

    updated deploy script

--- deploy_scripts/push_images.sh
+++ deploy_scripts/push_images.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 docker login -u $DOCKER_USER -p $DOCKER_API_KEY
-docker push battleground/uwsgi:latest
-docker push battleground/nginx:latest
-docker push battleground/uwsgi:latest
 docker push battleground/battleground:latest
