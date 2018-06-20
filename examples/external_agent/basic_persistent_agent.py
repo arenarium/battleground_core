edb43c78e234b1fd61cef1742e16ee1dddda02da
Vincent Lonij <vincentropy 29819815+vincentropy@users.noreply.github.com>
2018-06-20 10:25:44 +0100

    Updated documentation.

--- examples/external_agent/basic_persistent_agent.py
+++ examples/external_agent/basic_persistent_agent.py
@@ -4,21 +4,17 @@ from battleground.agent import Agent
 class PersistentAgent(Agent):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.my_turn_num = None
         self.default_mem = {"guess": 5}
 
     def move(self, state):
         memory = self.get_memory(default=self.default_mem)
-        self.my_turn_num = state["turn"]
-        return {"value": memory["guess"]}
+        # do something with memory here
+        my_move = {"value": memory["guess"]}
 
-    def observe(self, state):
-        memory = self.get_memory(default=self.default_mem)
-        # after initialization or if something made it forget:
+        # update memory
+        memory['key 1'] = 'value _1'
+        memory['key 2'] = 'value _2'
         self.set_memory(memory)
-        if state["turn"] - 1 == self.my_turn_num:
-            last_roll = state["last_roll"]
-            delta = last_roll - memory["guess"]
-            # improve guess based on last roll data
-            memory["guess"] += delta * 0.01
-            self.set_memory(memory)
+
+        # return move
+        return my_move
